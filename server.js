3d91d770da3b2b50ad05b1c41cbb251872e0da35
zero2323 <45566980+zero2323@users.noreply.github.com>
2019-04-02 15:21:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
   requireHeader: [],
+  setHeaders  : {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+  },
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
