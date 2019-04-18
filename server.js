c2884ba4c20239669471b7a0e27017d66b4c8a2f
zero2323 <45566980+zero2323@users.noreply.github.com>
2019-04-18 20:03:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ cors_proxy.createServer({
   originWhitelist: originWhitelist,
   requireHeader: [],
   setHeaders  : {
-        "user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"
+        "Access-Control-Expose-Headers": "ETag"
   },
   checkRateLimit: checkRateLimit,
   removeHeaders: [
